a45165e8e17464182deab33e3b59b14cfdfa7b77
kolucy <lucychan20d@gmail.com>
2022-10-05 15:03:01 +0900

    query annotation

--- shop/src/main/java/kr/ac/gachon/shop/repository/ItemRepository.java
+++ shop/src/main/java/kr/ac/gachon/shop/repository/ItemRepository.java
@@ -2,6 +2,30 @@ package kr.ac.gachon.shop.repository;
 
 import kr.ac.gachon.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemNm(String itemNm); // find+(Entity명)+By+(변수명)으로 메소드 생성. Entity명 제거할 수 있음
+    // List<Item> findItemByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price); // 입력된 price 미만 값 확인
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); // 입력된 price 미만 값 확인 및 price 값 기준 정렬
+
+    List<Item> findByPriceLessThanEqual(Integer price); // 입력된 price 이하 값 확인
+
+    List<Item> findByPriceIsBetween(Integer priceMin, Integer priceMax); // 입력된 price 사이 값 확인
+
+    @Query("select i from Item i where i.itemDetail like " +
+            "%:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail); // 직접 Query를 입력하여 값 확인 JPQL
+
+    @Query(value="select * from item i where i.item_detail like " +
+            "%:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail); // 직접 Query를 입력하여 값 확인 MySQL
 }
--- shop/src/test/java/kr/ac/gachon/shop/repository/ItemRepositoryTest.java
+++ shop/src/test/java/kr/ac/gachon/shop/repository/ItemRepositoryTest.java
@@ -9,10 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hibernate.tool.schema.SchemaToolingLogging.LOGGER;
 
 @SpringBootTest // 테스트 실행시 모든 Bean을 IoC 컨테이너에 등록 = 스프링부트 구동과 동일한 환경 조건
 @TestPropertySource(locations="classpath:application-test.properties") // 테스트 실행시 우선되는 설정파일
-public class ItemRepositoryTest {
+class ItemRepositoryTest {
     @Autowired // 필드 Bean 주입
     ItemRepository itemRepository;
 
@@ -29,5 +32,98 @@ public class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    public void createItemList(){
+        for(int i=1; i<=10; i++){
+            Item item = new Item();
+            item.setItemNm("좋은 상품 " + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("좋은 상품 설명 " + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100); item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 확인")
+    public void findByItemNmTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("좋은 상품 7");
+        for(Item item : itemList){
+            LOGGER.info(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 or 상품 설명 확인")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemList(); // 리스트 넣어준다
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("좋은 상품 1", "좋은 상품 설명 5"); // 추상메소드 itemRepository
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 확인")
+    public void findByPriceLessThanTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005); // price 변수 값이 10005 미만인 상품만 검색
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 확인")
+    public void findByPriceLessThanOrderByPriceDesc(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10006); // price 변수 가격이 10006 미만인 상품만 후 내림차순 정렬
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThanEqual 확인")
+    public void findByPriceLessThanEqual(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanEqual(10006);
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 IsBetween 확인")
+    public void findByPriceIsBetween(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceIsBetween(10002, 10005);
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 확인")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("좋은 상품");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 확인 NATIVE")
+    public void findByItemDetailByNativeTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("좋은 상품");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
     }
 }
