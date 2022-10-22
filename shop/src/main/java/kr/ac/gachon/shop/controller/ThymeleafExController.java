cd3f75b1161076dda9855d182ab16b167d605bbb
kolucy <lucychan20d@gmail.com>
2022-10-22 17:22:32 +0900

    th:text

--- shop/src/main/java/kr/ac/gachon/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/kr/ac/gachon/shop/controller/ThymeleafExController.java
@@ -1,9 +1,13 @@
 package kr.ac.gachon.shop.controller;
 
+import kr.ac.gachon.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+
 
 @Controller
 @RequestMapping(value="/thymeleaf")
@@ -13,5 +17,17 @@ public class ThymeleafExController {
         //http://localhost:8080/thymeleaf/ex01
         model.addAttribute("data", "타임리프 예제 입니다.");
         return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
     }
 }
