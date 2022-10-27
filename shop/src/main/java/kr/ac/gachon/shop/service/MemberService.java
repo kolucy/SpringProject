2466ec943bb5e70bc795baad554223d07248e871
kolucy <lucychan20d@gmail.com>
2022-10-27 17:58:15 +0900

    feat: login/logout service

--- shop/src/main/java/kr/ac/gachon/shop/service/MemberService.java
+++ shop/src/main/java/kr/ac/gachon/shop/service/MemberService.java
@@ -3,6 +3,10 @@ package kr.ac.gachon.shop.service;
 import kr.ac.gachon.shop.entity.Member;
 import kr.ac.gachon.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,7 +14,7 @@ import javax.transaction.Transactional;
 @Service
 @Transactional // 개발시에만 적용(rollback)
 @RequiredArgsConstructor // bean 주입 방법 생성자 final member, @NonNull member 생성자 생성함
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -24,5 +28,21 @@ public class MemberService {
         if(findMember != null) {
             throw new IllegalStateException("이미 가입된 회원입니다."); // 예외 처리
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
