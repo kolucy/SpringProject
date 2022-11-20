379434b5e97e33922e49eb03deb2df201f33e05c
kolucy <lucychan20d@gmail.com>
2022-11-20 17:17:12 +0900

    feat: complete s3 image upload(local)

--- shop/src/main/java/kr/ac/gachon/shop/service/FileService.java
+++ shop/src/main/java/kr/ac/gachon/shop/service/FileService.java
@@ -1,5 +1,6 @@
 package kr.ac.gachon.shop.service;
 
+/*
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 
@@ -32,4 +33,121 @@ public class FileService {
         }
     }
 
+}
+*/
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+
+
+@RequiredArgsConstructor
+@Service
+@Log
+public class FileService {
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @PostConstruct
+    public void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build();
+    }
+
+    public String mkFileName(String oriImgName, MultipartFile itemImgFile) {
+
+        //서로 다른 개체들을 구별하기 위해서는 이름을 부여해야 하므로 UUID(Universally Unique Identifier) 사용.(파일명 중복 문제 해결.)
+        UUID uuid = UUID.randomUUID();
+        //확장자를 찾기 위한 코드.
+        String extension = oriImgName.substring(oriImgName.lastIndexOf("."));
+        //UUID로 받은 값과 원래 파일의 이름의 확장자를 조합하여 저장될 파일 이름을 만듦.
+        String fileName = uuid.toString() + extension;
+        return fileName;
+    }
+
+    public String uploadFile(String oriImgName, MultipartFile itemImgFile) throws IOException {
+
+        //S3에 Multipartfile 타입은 전송이 안 되므로 MultiPartFile을 File로 전환.
+        File uploadFile = convert(itemImgFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환이 실패했습니다."));
+        String uploadImageUrl = putS3(uploadFile, mkFileName(oriImgName, itemImgFile));
+        removeNewFile(uploadFile);
+        //업로드된 파일의 S3 url 주소를 반환.
+        return uploadImageUrl;
+    }
+
+    public String putS3(File uploadFile, String fileName) throws IOException {
+
+        //외부에서 정적 파일을 읽을 수 있도록 하기 위해서, 전환된 File을 S3에 public 읽기 권한으로 업로드.
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        return s3Client.getUrl(bucket, fileName).toString();
+    }
+
+
+    //Multipartfile이 File로 전환되면서 로컬에 생성된 File을 삭제.
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if(convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
+
+    public void deleteFile(String filePath) throws Exception{
+
+        //파일이 저장된 경로를 이용하여 파일 객체를 생성.
+        File deleteFile = new File(filePath);
+
+        //해당 파일이 존재하면 파일을 삭제.
+        if(deleteFile.exists()){
+            deleteFile.delete();
+            log.info("파일을 삭제하였습니다.");
+        } else{
+            log.info("파일이 존재하지 않습니다.");
+        }
+    }
 }
--- shop/src/main/java/kr/ac/gachon/shop/service/ItemImgService.java
+++ shop/src/main/java/kr/ac/gachon/shop/service/ItemImgService.java
@@ -11,6 +11,7 @@ import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityNotFoundException;
 
+/*
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -56,5 +57,63 @@ public class ItemImgService {
             String imgUrl = "/images/item/" + imgName;
             savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
         }
+    }
+}
+*/
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemImgService {
+    @Value("${cloud.aws.s3.bucket.url}")
+    private String uploadPath;
+
+    private final ItemImgRepository itemImgRepository;
+
+    private final FileService fileService;
+
+    public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception{
+
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = "";
+        String imgUrl = "";
+
+        //파일 업로드
+        if(!StringUtils.isEmpty(oriImgName)){
+
+            //사용자가 상품의 이미지를 등록했다면 FileService 클래스의 mkFileName 메소드를 호출하여 변수 imgName에 저장.
+            imgName = fileService.mkFileName(oriImgName, itemImgFile);
+            //저장한 상품 이미지를 불러올 경로를 설정.
+            imgUrl = fileService.uploadFile(oriImgName, itemImgFile);
+        }
+
+        //상품 이미지 정보 저장
+        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        itemImgRepository.save(itemImg);
+    }
+
+    //상품 이미지를 수정한 경우 상품 이미지를 업데이트.
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception {
+
+        String oriImgName = itemImgFile.getOriginalFilename();
+
+        //상품 이미지 아이디를 이용하여 기존에 저장했던 상품 이미지 엔티티를 조회.
+        if (!itemImgFile.isEmpty()) {
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId)
+                    .orElseThrow(EntityNotFoundException::new);
+            //기존 이미지 파일 삭제
+            if (!StringUtils.isEmpty(savedItemImg.getImgName())) {
+                fileService.deleteFile(uploadPath + "/" + savedItemImg.getImgName());
+            }
+
+            //업데이트한 상품 이미지 파일을 업로드.
+            String imgName = fileService.mkFileName(oriImgName, itemImgFile);
+            String imgUrl = fileService.uploadFile(oriImgName, itemImgFile);
+            //변경된 상품 이미지 정보를 세팅.
+            //savedItemImg 엔티티는 현재 영속 상태이므로 itemImgRepository.save() 로직을 호출하지 않음.
+            //데이터를 변경하는 것만으로 변경 감지 기능이 동작하여 트랜잭션이 끝날 때 update 쿼리가 실행됨.
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        }
+
     }
 }
